0cf2cbcc61d0ace19de01ba27636355513da5a01
Giang Do <codegdo@gmail.com>
2021-09-19 20:43:17 -0700

    add schedule

--- react/src/components/calendar/calendar.schedule.tsx
+++ react/src/components/calendar/calendar.schedule.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { format, getMinutes, setDate } from "date-fns";
+import { format, getMinutes } from "date-fns";
 import { getIntervalTimes } from '../../helpers';
 import { CalendarContext } from './calendar.component';
 
@@ -19,8 +19,82 @@ export const CalendarSchedule: React.FC = (): JSX.Element => {
 
   return (
     <>
+      <div className="calendar-row">
+        <div className="calendar-column"></div>
+        {
+          resources.map((resource: any, i: number) => {
+            return <div key={i} className="calendar-column">{
+              resource.name
+            }</div>
+          })
+        }
+      </div>
 
-      <div className="schedule-row">
+      <div className="calendar-row">
+        <div className="calendar-column">
+          {
+            timeIntervals.map((interval: any, i: number) => {
+
+              return (getMinutes(interval) == 0) ? <div key={i} className="calendar-cell">
+                {
+                  format(interval, 'h a')
+                }
+              </div> : <div key={i} className="calendar-cell"></div>
+            })
+          }
+        </div>
+        {
+          resources.map((resource: any) => {
+
+            return <div key={resource.name} className="calendar-column">
+              {
+                timeIntervals.map((_interval: any, i: number) => {
+
+                  return <div key={i} className="calendar-cell"></div>
+                })
+              }
+            </div>
+
+          })
+        }
+
+      </div>
+
+    </>
+  )
+}
+
+/*
+<div className="calendar-row">
+      <div className="calendar-column">
+        {
+          timeIntervals.map((interval: any, i: number) => {
+
+            return (getMinutes(interval) == 0) && <div key={i}>
+              {
+                format(interval, 'h a')
+              }
+            </div>
+          })
+        }
+      </div>
+      {
+        resources.map((resource: any) => {
+          return <div className="calendar-column">
+            {
+              timeIntervals.map((interval: any, i: number) => {
+
+                return (getMinutes(interval) == 0) && <div key={i} className="calendar-cell">.</div>
+              })
+            }
+          </div>
+        })
+      }
+
+    </div>
+
+
+    <div className="schedule-row">
         <div className="schedule-cell">.</div>
         {
           resources.map((resource: any) => {
@@ -42,39 +116,4 @@ export const CalendarSchedule: React.FC = (): JSX.Element => {
           </div>
         })
       }
-
-    </>
-  )
-}
-
-/*
-<div className="calendar-row">
-      <div className="calendar-cell">
-        <div>All</div>
-        {
-          timeIntervals.map((interval: any, i: number) => {
-
-            return (getMinutes(interval) == 0) && <div key={i}>
-              {
-                format(interval, 'h a')
-              }
-            </div>
-          })
-        }
-      </div>
-      {
-        resources.map((resource: any) => {
-          return <div className="calendar-cell">
-            <div>{resource.name}</div>
-            {
-              timeIntervals.map((interval: any, i: number) => {
-
-                return (getMinutes(interval) == 0) && <div key={i}>.</div>
-              })
-            }
-          </div>
-        })
-      }
-
-    </div>
 */
