5b72e0aa846e6b8fb0ec900e793f0b6d01bb099c
Giang Do <codegdo@gmail.com>
2022-01-27 00:35:12 -0800

    save user

--- nestjs/src/database/main/02-user/sec.pr_user_signup.sql
+++ nestjs/src/database/main/02-user/sec.pr_user_signup.sql
@@ -1,6 +1,6 @@
 -- CREATE PROCEDURE USER_SIGNUP
 CREATE OR REPLACE PROCEDURE sec.pr_user_signup(
-  p_field_data jsonb,
+  p_field_data json,
 
   OUT data jsonb
 )
@@ -10,17 +10,11 @@ $BODY$
       _role_id INT := 2;
   BEGIN
 
-    -- convert field_data json to temp table tmp_field_data
+    -- convert field_data json to tmp_field_data
     DROP TABLE IF EXISTS tmp_field_data CASCADE;
-    CREATE TEMP TABLE tmp_field_data(
-      id int,
-      value varchar
-    );
-
-    INSERT INTO tmp_field_data(id, value)
-    VALUES 
-    (1, 'test'), 
-    (2, 'hello');
+    CREATE TEMP TABLE tmp_field_data AS
+    SELECT * FROM json_to_recordset(p_field_data)
+    AS rec ("id" int, "value" text);
 
     -- create a tmp_field
     DROP TABLE IF EXISTS tmp_field CASCADE;
@@ -37,7 +31,8 @@ $BODY$
 
     -- insert map and lookup
     UPDATE tmp_field
-    SET map = f.map
+    SET map = f.map,
+        lookup = f.lookup
     FROM org.field f
     WHERE tmp_field.id = f.id;
 
@@ -48,14 +43,14 @@ $BODY$
     ) tf;
 
     COMMIT;
-  END;
+  END
 $BODY$
 LANGUAGE plpgsql;
 
 
 
-CALL sec.pr_user_signup('{}', null);
+CALL sec.pr_user_signup('[{"id":1, "value":"gdo"},{"id":2, "value":"123"}]'::json, null);
 
-DROP PROCEDURE IF EXISTS sec.pr_sec_signup;
+DROP PROCEDURE IF EXISTS sec.pr_sec_signup CASCADE ;
 
 select * from org.field;
