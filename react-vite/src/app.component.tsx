2b675e65e6988e5b355862c870b5b84fbed789fd
Giang Do <codegdo@gmail.com>
2023-01-29 21:03:15 -0800

    update css

--- react-vite/src/app.component.tsx
+++ react-vite/src/app.component.tsx
@@ -1,6 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 //import { AppRoute } from './app.route';
 
+import * as styles from './assets/css/theme/theme.scss?inline';
+
 export const App: FC = (): JSX.Element => {
+
+  useEffect(() => {
+    //const scss = import('./assets/css/index.scss');
+
+    // void (async () => {
+    //   const scss: any = (await import('./assets/css/index.scss'));
+    //   console.log(scss);
+    // })();
+
+    console.log(styles.default);
+
+    //scss.then(data => console.log(data));
+
+  }, []);
+
   return <>hello</>;
 }
