93118f92fe368ce4f0d36285ea7b2e60e4c05a19
Giang Do <codegdo@gmail.com>
2023-02-15 23:43:41 -0800

    fix logout

--- react-vite/src/views/auth/logout/logout.page.tsx
+++ react-vite/src/views/auth/logout/logout.page.tsx
@@ -1,23 +1,25 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { http } from '../../../helpers';
 
 import { useAction, useFetch } from '../../../hooks';
+import { AppState } from '../../../store/reducers';
 
-const Logout: React.FC = (): JSX.Element => {
-
-  const { mutate: logout } = useFetch('/api/auth/logout');
-
-  const navigate = useNavigate();
+const Logout: React.FC = (): JSX.Element | null => {
+  const { loggedIn } = useSelector((state: AppState) => state.session);
   const { deleteSession } = useAction();
+  const { mutate: logout } = useFetch('/api/auth/logout');
+  const navigate = useNavigate();
 
   useEffect(() => {
-    logout();
-    deleteSession();
+    if (loggedIn) {
+      deleteSession();
+      logout();
+    }
     navigate('/login');
-  }, []);
+  }, [loggedIn]);
 
-  return <></>
+  return null;
 }
 
 export default Logout;
