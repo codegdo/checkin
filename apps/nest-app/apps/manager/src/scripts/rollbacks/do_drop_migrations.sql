0deeedc5948c14b6b0ea4431f9512556945b8490
Giang Do <gdo@Giangs-MacBook-Pro.local>
2023-09-11 06:49:17 -0700

    update

--- apps/nest-app/apps/manager/src/scripts/rollbacks/do_drop_migrations.sql
+++ apps/nest-app/apps/manager/src/scripts/rollbacks/do_drop_migrations.sql
@@ -18,3 +18,46 @@ BEGIN
     END IF;
   END LOOP;
 END $$;
+
+DO $$ 
+BEGIN
+  IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'migrations') THEN
+    DROP TABLE IF EXISTS main_sys.migrations
+  END IF;
+END $$;
+
+DO $$ 
+BEGIN
+  DROP TABLE IF EXISTS main_sys.migrations;
+END $$;
+
+DO $$ 
+DECLARE
+  table_exists BOOLEAN;
+BEGIN
+  -- Check if the table exists
+  SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'main_sys' AND table_name = 'migrations') INTO table_exists;
+  
+  -- If the table exists, attempt to drop it
+  IF table_exists THEN
+    BEGIN
+      -- Savepoint for rollback
+      SAVEPOINT drop_table_savepoint;
+      
+      -- Attempt to drop the table
+      DROP TABLE main_sys.migrations;
+      
+      -- If successful, release the savepoint (commit)
+      RELEASE SAVEPOINT drop_table_savepoint;
+    EXCEPTION
+      WHEN others THEN
+        -- Handle the exception here (you can log an error, raise an alert, etc.)
+        RAISE NOTICE 'Error dropping table: %', SQLERRM;
+        
+        -- Rollback to the savepoint in case of an error
+        ROLLBACK TO SAVEPOINT drop_table_savepoint;
+    END;
+  END IF;
+END $$;
+
+
