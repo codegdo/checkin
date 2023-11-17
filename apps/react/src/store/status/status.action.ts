1b5952e5908b20ef6905610ca2c7718c7eb4aea6
codegdo <codegdo@gmail.com>
2023-11-17 15:30:08 -0800

    remove status

--- apps/react/src/store/status/status.action.ts
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@reduxjs/toolkit';
-import { StatusState } from './status.type';
-
-export const updateStatus = createAction<StatusState>(
-  'status/UPDATE'
-);
-export const logoutStatus = createAction('status/LOGOUT');
--- apps/react/src/store/status/status.type.ts
+++
@@ -1,9 +0,0 @@
-import { AppStatus } from '@/constants';
-
-export interface StatusState {
-  appId?: number | null;
-  clientId: number | null;
-  current: AppStatus;
-  isLoggedIn: boolean;
-  userType: string | null;
-}
