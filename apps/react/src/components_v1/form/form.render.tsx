5ac50b7525c97d76dd4a2a4b63efc0dd922db22f
codegdo <codegdo@gmail.com>
2024-01-09 15:41:39 -0800

    add key to field

--- apps/react/src/components_v1/form/form.render.tsx
+++ apps/react/src/components_v1/form/form.render.tsx
@@ -14,6 +14,7 @@ export function FormRender({ data }: IFormRenderProps) {
 
 function renderData(data: ComponentField[] = []) {
   return data.map((item) => {
-    return <FormField key={item.id} />
+    const key = item.id || item.name;
+    return <FormField key={key} />
   });
 }
