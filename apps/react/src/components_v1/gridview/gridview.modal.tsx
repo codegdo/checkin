98df5fe65606af6f616fda2346a3c616de4083ac
Giang Do <gdo@Giangs-MacBook-Pro.local>
2024-01-25 06:35:02 -0800

    clean up

--- apps/react/src/components_v1/gridview/gridview.modal.tsx
+++ apps/react/src/components_v1/gridview/gridview.modal.tsx
@@ -1,4 +1,4 @@
-import { useApi } from "@/hooks";
+
 import { ContextValue } from "./contexts";
 import { ActionType } from "./types";
 import { useEffect } from "react";
@@ -9,14 +9,13 @@ interface IProps {
 
 export function GridViewModal({ context }: IProps) {
   const { state, onClick } = context;
-  const { query } = useApi(state.modal?.action);
 
   const handleClose = () => {
     onClick && onClick(ActionType.CLOSE);
   }
 
   useEffect(() => {
-    query && query();
+
   }, []);
 
   return (
